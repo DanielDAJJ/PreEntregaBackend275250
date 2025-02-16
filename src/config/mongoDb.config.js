85d963d8887dc0920490959c65fff1a26c1f63b5
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-02-15 20:32:30 -0500

    Se comienza con el proyecto para agregar la ruta de usuarios y que se utilicen las instancias de passport y JWT

--- src/config/mongoDb.config.js
+++ src/config/mongoDb.config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export  const connectMongoDB = async () => {
     try {
-        mongoose.connect('mongodb+srv://danieldajj:Drie950211@danieljaimescoderhouse.khe3r.mongodb.net/EntregaFinal')
+        mongoose.connect('mongodb+srv://danieldajj:Drie950211@danieljaimescoderhouse.khe3r.mongodb.net/PreEntregaBackend275250')
     } catch (error) {
         console.log(error);
     }
